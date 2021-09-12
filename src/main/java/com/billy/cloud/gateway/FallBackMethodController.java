61c9e0d91ca1c1f9477e3ea1726122fdd4d708b3
audubilly <audubilly89@gmail.com>
2021-09-12 01:56:20 +0100

    cloud-config-server & bootstrap.yml

--- src/main/java/com/billy/cloud/gateway/FallBackMethodController.java
+++ src/main/java/com/billy/cloud/gateway/FallBackMethodController.java
@@ -1,4 +1,19 @@
 package com.billy.cloud.gateway;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBackMethod")
+    public String userServiceFallBackMethood(){
+        return "User service is taking longer than Expected." + "Please try agian later";
+    }
+
+    @GetMapping("/departmetServiceFallBackMethod")
+    public String departmentServiceFallBackMethood(){
+        return "Department service is taking longer than Expected." + "Please try agian later";
+    }
 }
